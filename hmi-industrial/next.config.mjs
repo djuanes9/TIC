09cc91188e2fcdffc88e746f180fbd504e461d10
Juan Armas <juan.armas01@epn.edu.ec>
2024-09-19 19:10:59 -0500

    nuevo

--- hmi-industrial/next.config.mjs
+++ hmi-industrial/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true,  // Asegúrate de que el experimental appDir esté habilitado
+    },
+  }
+  
+  module.exports = nextConfig;
+  
