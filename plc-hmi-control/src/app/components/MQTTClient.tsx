c85bae7832945d52b4022276bd4903b08b5ba9fb
Juan Armas <juan.armas01@epn.edu.ec>
2024-09-21 20:47:27 -0500

    MQTT

--- plc-hmi-control/src/app/components/MQTTClient.tsx
+++ plc-hmi-control/src/app/components/MQTTClient.tsx
@@ -1,9 +1,15 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import mqtt from 'mqtt';
+import React, { useEffect, useState, useRef } from 'react';
+import mqtt, { MqttClient }  from 'mqtt';
 
 const MQTTClient = () => {
   const [isConnected, setIsConnected] = useState(false);
+    
+  // Definimos que clientRef puede ser de tipo MqttClient o null
+  const clientRef = useRef<MqttClient  | null>(null);
+
+
+
 
   useEffect(() => {
     const client = mqtt.connect('wss://260739b4dbf540efbb87cd6f024aa9f0.s1.eu.hivemq.cloud:8884/mqtt', {
@@ -14,14 +20,18 @@ const MQTTClient = () => {
       connectTimeout: 30 * 1000, // Tiempo de espera de conexión
     });
 
+    clientRef.current = client; // Almacenamos el cliente en la referencia
+
+
     client.on('connect', () => {
       console.log('Connected to broker');
       setIsConnected(true);
 
-      // Suscribirse a un tópico
-      client.subscribe('test/topic', (err) => {
+      // Suscribirse a los tópicos
+      const topics = ['valvula', 'motor1', 'molino', 'motor2'];
+      client.subscribe(topics, (err) => {
         if (!err) {
-          console.log('Subscribed successfully');
+       //   console.log(Subscribed to topics: ${topics.join(', ')});
         } else {
           console.error('Subscription error: ', err);
         }
@@ -34,7 +44,8 @@ const MQTTClient = () => {
     });
 
     client.on('message', (topic, message) => {
-      console.log(`Message received from ${topic}: ${message.toString()}`);
+    //  console.log(Message received from ${topic}: ${message.toString()});
+      // Aquí puedes manejar cada mensaje según el tópico
     });
 
     return () => {
@@ -42,12 +53,34 @@ const MQTTClient = () => {
     };
   }, []);
 
+  // Funciones para publicar mensajes
+  const publishStart = () => {
+    if (isConnected) {
+   //   clientRef.publish('start', '1');
+      console.log('Published to start');
+    } else {
+      console.error('Client not connected');
+    }
+  };
+
+  const publishStop = () => {
+    if (isConnected) {
+    //  clientRef.publish('stop', '1');
+      console.log('Published to stop');
+    } else {
+      console.error('Client not connected');
+    }
+  };
+
   return (
     <div>
       <h2>MQTT Client</h2>
       <p>Status: {isConnected ? 'Connected' : 'Disconnected'}</p>
+      <button onClick={publishStart} disabled={!isConnected}>Start</button>
+      <button onClick={publishStop} disabled={!isConnected}>Stop</button>
     </div>
   );
 };
+
 
 export default MQTTClient;
