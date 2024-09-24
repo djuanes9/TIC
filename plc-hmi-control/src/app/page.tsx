c9b79950dec1b62dbe1fcdff64f87b85466602bc
Juan Armas <juan.armas01@epn.edu.ec>
2024-09-24 08:46:51 -0500

    QWQ

--- plc-hmi-control/src/app/page.tsx
+++ plc-hmi-control/src/app/page.tsx
@@ -3,7 +3,6 @@ import MQTTClient from './components/MQTTClient';
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-3xl font-bold mb-8">Grain Milling Process Control</h1>
       <MQTTClient />
     </div>
   );
