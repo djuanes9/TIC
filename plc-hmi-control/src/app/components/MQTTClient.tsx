90d364310faf390fd5ea93cd4548a50b6fc051ac
Juan Armas <juan.armas01@epn.edu.ec>
2024-09-23 20:52:53 -0500

    rrr

--- plc-hmi-control/src/app/components/MQTTClient.tsx
+++ plc-hmi-control/src/app/components/MQTTClient.tsx
@@ -4,9 +4,9 @@ import React, { useEffect, useState } from "react";
 import mqtt, { MqttClient } from "mqtt";
 
 // Importa tus imágenes SVG
-import SiloSvg from "./public/silo.svg";  // Ruta al SVG del Silo
-import ConveyorSvg from "./public/conveyor.svg";  // Ruta al SVG del Conveyor
-import MillSvg from "./public/mill.svg";  // Ruta al SVG del Molino
+import SiloSvg from "../public/silo.svg";  // Ruta al SVG del Silo
+import ConveyorSvg from "../public/conveyor.svg";  // Ruta al SVG del Conveyor
+import MillSvg from "../public/mill.svg";  // Ruta al SVG del Molino
 
 interface Message {
   topic: string;
