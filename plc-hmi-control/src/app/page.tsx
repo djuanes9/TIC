62b42bff3b8c18cb943d3ab35c7c79157fb15789
Juan Armas <168566504+djuanes9@users.noreply.github.com>
2024-09-28 10:20:14 -0500

    Update page.tsx

--- plc-hmi-control/src/app/page.tsx
+++ plc-hmi-control/src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import "./globals.css";
-import ReactDOM from "react-dom/client";  // Cambia la importación a 'react-dom/client'
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);  // Usa 'createRoot'
-root.render(<App />);  // Renderiza el componente usando 'root.render'
+
+export default function HomePage() {
+  return <App />;
+}
