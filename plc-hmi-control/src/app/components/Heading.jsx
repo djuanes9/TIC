362bb4ff7f06ad48a57830b198deaa82cc408716
Juan Armas <juan.armas01@epn.edu.ec>
2024-09-30 01:55:57 -0500

    ee

--- plc-hmi-control/src/app/components/Heading.jsx
+++ plc-hmi-control/src/app/components/Heading.jsx
@@ -5,10 +5,12 @@ const imgEPN = "https://daci.epn.edu.ec/images/inicio/logo_home_w.png";
 function Heading() {
   return (
     <header>
+      <div>
       <h1 contentEditable="false" spellCheck="false">
         ESCUELA POLITÉCNICA NACIONAL
       </h1>
       <h2>Trabajo de integración Curricular</h2>
+      </div>
       <img className="circle-img " alt="EP" src={imgEPN} />
     </header>
   );
