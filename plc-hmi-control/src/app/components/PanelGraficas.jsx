5b48fc6fd3d5821f43e8c7a5f68856a608491d4f
Juan Armas <juan.armas01@epn.edu.ec>
2024-10-08 10:43:28 -0500

    sasas

--- plc-hmi-control/src/app/components/PanelGraficas.jsx
+++ plc-hmi-control/src/app/components/PanelGraficas.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MQTTContext } from "./MQTTCliente"; // Importamos el contexto
 
 const PanelGraficas = () => {
   const { statuses, isConnected, sendMessage, isNodeRedConnected } = useContext(MQTTContext); // Usamos los valores globales
+  const [bandaValue, setBandaValue] = useState(0); // Estado local para el valor de SP_banda
 
+  // Función para manejar los pulsadores
   const handleButtonPress = (topic) => {
     // Publicar el valor "true" o "ON" en el tópico MQTT
     sendMessage(topic, "true");
@@ -14,10 +16,19 @@ const PanelGraficas = () => {
     }, 1000); // 1000 ms = 1 segundo
   };
 
+  // Función para manejar el envío del valor de la banda
+  const handleBandaValueChange = (event) => {
+    setBandaValue(event.target.value); // Actualiza el valor localmente
+  };
+
+  // Función para enviar el valor de SP_banda
+  const handleSendBandaValue = () => {
+    sendMessage("SP_banda", bandaValue); // Enviar el valor actual de bandaValue al tópico SP_banda
+  };
+
   return (
     <div>
       <p>Status de conexión: {isConnected ? "Conectado" : "Desconectado"}</p>
-      {/* Mostrar estado de Node-RED */}
       <p>Status de Node-RED: {isNodeRedConnected ? "Conectado" : "Desconectado"}</p>
 
       <div>
@@ -34,6 +45,7 @@ const PanelGraficas = () => {
             <img src="/LedOff.png" alt="LED apagado" />
           )}
         </div>
+
         <div className="StatusItem">
           <h3>MILL-101</h3>
           {statuses["MILL-101"] === "ON" ? (
@@ -75,6 +87,21 @@ const PanelGraficas = () => {
         >
           STOP
         </button>
+      </div>
+
+      {/* Input para seleccionar el valor de 0 a 100 y enviar al tópico SP_banda */}
+      <div className="bandaControl">
+        <label htmlFor="bandaRange">Set Point Banda (0-100):</label>
+        <input
+          id="bandaRange"
+          type="range"
+          min="0"
+          max="100"
+          value={bandaValue}
+          onChange={handleBandaValueChange} // Actualizar el estado local al mover el slider
+        />
+        <span>{bandaValue}</span> {/* Mostrar el valor actual */}
+        <button onClick={handleSendBandaValue}>Enviar Valor Banda</button> {/* Botón para enviar el valor */}
       </div>
     </div>
   );
