262f37399b9d83aa84d997b25ffd6c489e3beee9
Juan Armas <juan.armas01@epn.edu.ec>
2025-01-06 06:59:21 -0500

    info

--- plc-hmi-control/src/app/components/Informacion.jsx
+++ plc-hmi-control/src/app/components/Informacion.jsx
@@ -1,38 +1,230 @@
-import React, { useState } from "react";
-import "./Login.css";
-function Login({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+import React, { useState, useEffect, useContext } from "react";
+import { MQTTContext } from "./MQTTCliente";
+import "./Info.css";
 
-  const handleSubmit = (e) => {
+const Informacion = () => {
+  const { sendMessage, statuses } = useContext(MQTTContext);
+
+  const [configuracion, setConfiguracion] = useState({
+    NameProdInicio: "Grano Descascarillado",
+    NameProdFinal: "Grano Molido",
+    NivelSiloMin: 10,
+    NivelSiloMax: 90,
+    CalidadIdeal: 90,
+    TiempoPlanificado: 60,
+    VelocidadOperacion: 20,
+  });
+
+  const [configuracionActual, setConfiguracionActual] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (statuses["configuracion/salida"]) {
+      try {
+        let data = statuses["configuracion/salida"];
+
+        if (typeof data === "string") {
+          data = JSON.parse(data);
+        }
+
+        if (Array.isArray(data) && data.length > 0) {
+          setConfiguracionActual(data[0]);
+        } else {
+          console.warn("El JSON recibido está vacío o no es un array válido.");
+          setConfiguracionActual(null);
+        }
+      } catch (error) {
+        console.error("Error al procesar configuracion/salida:", error);
+        setConfiguracionActual(null);
+      }
+    }
+  }, [statuses]);
+
+  const obtenerFechaYHora = () => {
+    const now = new Date();
+    return {
+      hora: now.toTimeString().split(" ")[0],
+      dia: now.getDate(),
+      mes: now.getMonth() + 1,
+      anio: now.getFullYear(),
+    };
+  };
+
+  const handleSolicitarUltimaConfig = () => {
+    sendMessage("request/config", "Solicitar última configuración");
+
+    setTimeout(() => {
+      if (statuses["configuracion/salida"] && configuracionActual) {
+        setShowModal(true);
+      } else {
+        alert("No se han recibido datos. Por favor, intente nuevamente.");
+      }
+    }, 1200);
+  };
+
+  const handleCerrarModal = () => {
+    setShowModal(false);
+  };
+
+  const handleActualizar = (e) => {
     e.preventDefault();
-    onLogin(username, password); // Llama a la función de autenticación
+    const fechaYHora = obtenerFechaYHora();
+
+    const configuracionConFecha = {
+      ...configuracion,
+      ...fechaYHora,
+    };
+    sendMessage("configuracion/entrada", JSON.stringify(configuracionConFecha));
+    alert("Configuración actualizada correctamente.");
+  };
+
+  const handleChange = (e) => {
+    const { name, value, type } = e.target;
+
+    setConfiguracion((prevConfig) => ({
+      ...prevConfig,
+      [name]: type === "number" ? parseFloat(value) || 0 : value,
+    }));
   };
 
   return (
-    <div className="login-container">
-      <h2>Iniciar Sesión</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Usuario:</label>
+    <div className="form-container">
+      <h2>Configuración del Sistema</h2>
+
+      <form className="config-form">
+        <div className="form-group">
+          <label>Nombre del Producto Inicial:</label>
           <input
             type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            name="NameProdInicio"
+            value={configuracion.NameProdInicio}
+            onChange={(e) =>
+              setConfiguracion({
+                ...configuracion,
+                NameProdInicio: e.target.value,
+              })
+            }
           />
         </div>
-        <div>
-          <label>Contraseña:</label>
+        <div className="form-group">
+          <label>Nombre del Producto Final:</label>
           <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            name="NameProdFinal"
+            value={configuracion.NameProdFinal}
+            onChange={(e) =>
+              setConfiguracion({
+                ...configuracion,
+                NameProdFinal: e.target.value,
+              })
+            }
           />
         </div>
-        <button type="submit">Ingresar</button>
+        <div className="form-group">
+          <label>Nivel Silo Mínimo:</label>
+          <input
+            type="number"
+            name="NivelSiloMin"
+            value={configuracion.NivelSiloMin}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Nivel Silo Máximo:</label>
+          <input
+            type="number"
+            name="NivelSiloMax"
+            value={configuracion.NivelSiloMax}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Calidad Ideal:</label>
+          <input
+            type="number"
+            name="CalidadIdeal"
+            value={configuracion.CalidadIdeal}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Tiempo Planificado (minutos):</label>
+          <input
+            type="number"
+            name="TiempoPlanificado"
+            value={configuracion.TiempoPlanificado}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Velocidad de Operación (gramos/min):</label>
+          <input
+            type="number"
+            name="VelocidadOperacion"
+            value={configuracion.VelocidadOperacion}
+            onChange={handleChange}
+          />
+        </div>
       </form>
+      <button className="btn-submit" type="button" onClick={handleActualizar}>
+        Actualizar Configuración
+      </button>
+      <button className="btn-request" onClick={handleSolicitarUltimaConfig}>
+        Ver Configuración Actual
+      </button>
+
+      {showModal && configuracionActual && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <h3>Última Configuración</h3>
+            <table className="config-table">
+              <tbody>
+                <tr>
+                  <td>Producto Inicial:</td>
+                  <td>{configuracionActual.NameProdInicio}</td>
+                </tr>
+                <tr>
+                  <td>Producto Final:</td>
+                  <td>{configuracionActual.NameProdFinal}</td>
+                </tr>
+                <tr>
+                  <td>Nivel Silo Mínimo:</td>
+                  <td>{configuracionActual.NivelSiloMin}</td>
+                </tr>
+                <tr>
+                  <td>Nivel Silo Máximo:</td>
+                  <td>{configuracionActual.NivelSiloMax}</td>
+                </tr>
+                <tr>
+                  <td>Calidad Ideal:</td>
+                  <td>{configuracionActual.CalidadIdeal}</td>
+                </tr>
+                <tr>
+                  <td>Tiempo Planificado:</td>
+                  <td>{configuracionActual.TiempoPlanificado} minutos</td>
+                </tr>
+                <tr>
+                  <td>Velocidad de Operación:</td>
+                  <td>{configuracionActual.VelocidadOperacion} gramos/min</td>
+                </tr>
+                <tr>
+                  <td>Hora:</td>
+                  <td>{configuracionActual.hora}</td>
+                </tr>
+                <tr>
+                  <td>Fecha:</td>
+                  <td>{`${configuracionActual.dia}/${configuracionActual.mes}/${configuracionActual.anio}`}</td>
+                </tr>
+              </tbody>
+            </table>
+            <button className="btn-close" onClick={handleCerrarModal}>
+              Cerrar
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default Login;
+export default Informacion;
